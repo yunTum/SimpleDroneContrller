86f129921f176d594c8fcacfdccc12a57595d501
yunTum <yusukeaonuma616@gmail.com>
2023-08-30 23:42:56 +0900

    気体の状態のリアルタイム表示対応

--- gui.py
+++ gui.py
@@ -4,13 +4,19 @@ import tello_manager
 import time
 import threading
 import PySimpleGUI as sg
+import re
 
 class TelloController:
   def __init__(self, mode):
-    self.tello = tello_manager.TelloSocket(8889, mode)
-    # Tello SDK mode
-    self.tello.socket_send('command')
+    self.state = 'init'
+    self.mode = mode
+    
     self.window = None
+    self.recv_thread = None
+    self.query_thread = None
+    self.update_thread = None
+    self.create_window()
+    self.run()
 
   def create_window(self):
     sg.theme('DarkGrey5')
@@ -33,6 +39,12 @@ class TelloController:
         [sg.Text('Socket State')],
         [sg.Text('Socket:'), sg.Text('disconneted', key='-SOCKET-')],
       ], size=(100,100)
+    )
+    connect_frame = sg.Frame('',
+      [
+        [sg.Text('Connect')],
+        [sg.Button('Connect', key='-CONNECT-'), sg.Button('Disconnect', key='-DISCONNECT-')],
+      ], pad=((20, 0), ( 0, 50))
     )
     flight_flame = sg.Frame('',
       [
@@ -57,6 +69,7 @@ class TelloController:
     
     control_frame = sg.Frame('',
       [
+        [connect_frame],
         [flight_flame],
         [move_frame],
       ], relief=sg.RELIEF_FLAT
@@ -66,16 +79,51 @@ class TelloController:
                 [state_frame, control_frame],
               ]
 
-    self.window = sg.Window('Tello Controller', layout, resizable=True)
+    self.window = sg.Window('Tello Controller', layout, resizable=True, finalize=True)
+  
+  def tello_connect(self):
+    try:
+      self.tello = tello_manager.TelloSocket(8889, self.mode)
+      self.tello.socket_setup()
+      self.state = self.tello.get_state()
+      self.window['-SOCKET-'].update(self.state)
+      if self.state == 'connected':
+        self.tello_state = tello_manager.Tello()
+        self.window['-CONNECT-'].update(disabled=True)
+        self.window['-DISCONNECT-'].update(disabled=False)
+        # Tello SDK mode
+        self.tello.socket_send('command')
+        self.recv_thread = threading.Thread(target=self.recv_tello)
+        self.recv_thread.start()
+        
+        self.query_thread = threading.Thread(target=self.query_tello)
+        self.query_thread.start()
+        
+        self.update_thread = threading.Thread(target=self.update_window)
+        self.update_thread.start()
+    except Exception as ex:
+      print('TelloController: {0}'.format(str(ex)) )
+      self.tello = None
+  
+  def tello_disconnect(self):
+    self.tello.socket_close()
+    self.state = self.tello.get_state()
+    self.recv_thread.join()
+    self.query_thread.join()
+    self.update_thread.join()
+    self.window['-CONNECT-'].update(disabled=False)
+    self.window['-DISCONNECT-'].update(disabled=True)
+    self.window['-SOCKET-'].update(self.state)
   
   def run(self):
-  
     while True:
         event, values = self.window.read()
         if event == sg.WIN_CLOSED:
-          self.tello.socket_send('quit')
-          self.tello.change_state(self.tello.STATE_STR_QUIT)
           break
+        if event == '-CONNECT-':
+          self.tello_connect()
+        if event == '-DISCONNECT-':
+          self.tello_disconnect()
         if event == '-TAKEOFF-':
           self.event_takeoff()
         if event == '-LAND-':
@@ -93,39 +141,94 @@ class TelloController:
         if event == '-RIGHTROLL-':
           self.event_rightroll()
 
+    self.kill_thread()
     self.window.close()
+  
+  def kill_thread(self):
+    self.tello.socket_close()
+    self.state = self.tello.get_state()
+    self.recv_thread.join()
+    self.query_thread.join()
+    self.update_thread.join()
+  
+  def recv_tello(self):
+    response = None
+    while self.state != 'disconnected':
+      response = self.tello.recv_data()
+      split_response = re.split('[:;]', response)
+      if 'battery' in split_response:
+        self.tello_state.set_battery(split_response[1])
+      if 'vgx' in split_response:
+        self.tello_state.set_speed(split_response[1], split_response[3], split_response[5])
+      if 'pitch' in split_response:
+        self.tello_state.set_attitude(split_response[1], split_response[3], split_response[5])
+      if 'baro' in split_response:
+        self.tello_state.set_barometer(split_response[1])
+      if 'tof' in split_response:
+        self.tello_state.set_tof(split_response[1])
+      if 'height' in split_response:
+        self.tello_state.set_height(split_response[1])
+      if 'agx' in split_response:
+        self.tello_state.set_acceleration(split_response[1], split_response[3], split_response[5])
+
+    print('exit from the recv thread.')
+    
+  def query_tello(self):
+    while self.state != 'disconnected':
+      self.tello.send_query()
+
+  def update_window(self, ):
+    while self.state != 'disconnected':
+      self.window['-BATTERY-'].update(self.tello_state.battery)
+      self.window['-XSPEED-'].update(self.tello_state.speed['vgx'])
+      self.window['-YSPEED-'].update(self.tello_state.speed['vgy'])
+      self.window['-ZSPEED-'].update(self.tello_state.speed['vgz'])
+      self.window['-ROLL-'].update(self.tello_state.attitude['roll'])
+      self.window['-PITCH-'].update(self.tello_state.attitude['pitch'])
+      self.window['-BAROMETER-'].update(self.tello_state.barometer)
+      self.window['-TOF-'].update(self.tello_state.tof)
+      self.window['-HEIGHT-'].update(self.tello_state.height)
+      time.sleep(0.5)
   
   def event_takeoff(self):
     print('TAKEOFF')
-    self.tello.socket_send('takeoff')
+    if self.state == 'connected':
+      self.tello.socket_send('takeoff')
   
   def event_land(self):
     print('LAND')
-    self.tello.socket_send('land')
+    if self.state == 'connected':
+      self.tello.socket_send('land')
   
   def event_front(self):
     print('FRONT')
-    self.tello.socket_send('forward 20')
+    if self.state == 'connected':
+      self.tello.socket_send('forward 20')
   
   def event_back(self):
     print('BACK')
-    self.tello.socket_send('back 20')
+    if self.state == 'connected':
+      self.tello.socket_send('back 20')
   
   def event_left(self):
     print('LEFT')
-    self.tello.socket_send('left 20')
+    if self.state == 'connected':
+      self.tello.socket_send('left 20')
   
   def event_right(self):
     print('RIGHT')
-    self.tello.socket_send('right 20')
+    if self.state == 'connected':
+      self.tello.socket_send('right 20')
   
   def event_leftroll(self):
     print('LEFTROLL')
-    self.tello.socket_send('ccw 20')
+    if self.state == 'connected':
+      self.tello.socket_send('ccw 20')
   
   def event_rightroll(self):
     print('RIGHTROLL')
-    self.tello.socket_send('cw 20')
+    if self.state == 'connected':
+      self.tello.socket_send('cw 20')
 
 if __name__ == "__main__":
   pass
