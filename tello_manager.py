da4d8fdd390640de62ea49a6b604dd27f8c7c7b7
yunTum <yusukeaonuma616@gmail.com>
2023-08-30 23:42:09 +0900

    ソケット通信部分の変更と機体の状態管理オブジェクトの追加

--- tello_manager.py
+++ tello_manager.py
@@ -2,7 +2,6 @@
 
 import socket
 import time
-import threading
 import command_list
 
 '''
@@ -10,87 +9,84 @@ import command_list
 # address = 192.168.10.1 UDP PORT 8889
 '''
 class TelloSocket:
-  STATE_STR_DISCONNECTED = 'disconnected'
-  STATE_STR_CONNECTED = 'connected'
-  STATE_STR_CONNECTING = 'connecting'
-  STATE_STR_QUIT = 'quit'
   TELLO_IP_ADDRESS = '192.168.10.1'
   TEST_IP_ADDRESS = 'localhost'
   MY_IP_ADDRESS = '192.168.10.2'
   MY_PORT = 9000
   
   def __init__(self, port, *mode):
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    self.tello_address = (self.TELLO_IP_ADDRESS, port)
-    self.my_address = (self.MY_IP_ADDRESS, self.MY_PORT)
     self.mode = mode
-    if 'testmode' in self.mode:
-      print(self.mode[0])
-      self.tello_address = (self.TEST_IP_ADDRESS, 9999)
-      self.my_address = (self.TEST_IP_ADDRESS, 9998)
-    
-    self.socket.bind(self.my_address)
-    self.socket.settimeout(2)
+    self.port = port
     self.udp_bufsize = 1024
-    self.state = self.STATE_STR_CONNECTED
-    
-    self.thead_recv = threading.Thread(target=self.recv_data)
-    self.thead_recv.start()
-
-    self.thead_query = threading.Thread(target=self.send_query)
-    self.thead_query.start()
+    self.state = 'disconnected'
+    self.response = None
 
   def __del__(self):
     print('TelloSocket is deleted')
-    
+  
+  def socket_setup(self):
+    try:
+      self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      self.tello_address = (self.TELLO_IP_ADDRESS, self.port)
+      self.my_address = (self.MY_IP_ADDRESS, self.MY_PORT)
+      if 'testmode' in self.mode:
+        print(self.mode[0])
+        self.mode = self.mode[0]
+        self.tello_address = (self.TEST_IP_ADDRESS, 9999)
+        self.my_address = (self.TEST_IP_ADDRESS, 9998)
+      
+      self.socket.bind(self.my_address)
+      self.state = 'connected'
+      self.socket.settimeout(2)
+    except Exception as ex:
+      print('socket failed: {0}'.format(str(ex)) )
+      self.state = 'failed'
+  
   def socket_send(self, command):
       self.socket.sendto(command.encode('utf-8'), self.tello_address)
   
   def recv_data(self):
-    timeout_count = 0
-    while self.state != self.STATE_STR_QUIT:
-      try:
-          response, ip_address = self.socket.recvfrom(self.udp_bufsize)
-          response = response.decode('utf-8')
-          print("recv: {0}".format(response) )
-          if response:
-            timeout_count = 0
-      except Exception as ex:
-          print('recv: {0}'.format(str(ex)) )
-          timeout_count += 1
-          if timeout_count > 5:
-            print('over timeout count')
-            break
-    print('exit from the recv thread.')
+    try:
+        response, ip_address = self.socket.recvfrom(self.udp_bufsize)
+        response = response.decode('utf-8')
+        if response:
+          self.response = response
+    except Exception as ex:
+        print('recv: {0}'.format(str(ex)) )
+        self.response = ""
+        self.state = 'failed'
+    return response
   
   def send_query(self):
-    while self.state != self.STATE_STR_QUIT:
-      try:
-        self.socket_send(command_list.query_battery)
-      except Exception as ex:
-        print('fail query', ex)
+      self.socket_send(command_list.query_battery)
       time.sleep(0.1)
-      try:
-        self.socket_send(command_list.query_attitude)
-      except Exception as ex:
-        print('fail query', ex)
+      self.socket_send(command_list.query_attitude)
       time.sleep(0.1)
-      try:
-        self.socket_send(command_list.query_tof)
-      except Exception as ex:
-        print('fail query', ex)
+      self.socket_send(command_list.query_tof)
       time.sleep(0.1)
+      self.socket_send(command_list.query_baro)
+      time.sleep(0.1)
+      self.socket_send(command_list.query_height)
+      time.sleep(0.1)
+      self.socket_send(command_list.qury_acceleration)
+      time.sleep(0.1)
+      self.socket_send(command_list.query_speed)
+      time.sleep(0.1)
+      
+  def get_response(self):
+    return self.response
   
-  def change_state(self, state):
+  def set_state(self, state):
     self.state = state
   
+  def get_state(self):
+    return self.state
+  
   def socket_close(self):
-    self.state = self.STATE_STR_QUIT
     if self.mode == 'testmode':
       self.socket.sendto('quit'.encode('utf-8'), self.tello_address)
-    self.thead_recv.join()
-    self.thead_query.join()
     self.socket.close()
+    self.state = 'disconnected'
 
 def drone_test():
   tello_port = 8889
@@ -104,6 +100,46 @@ def drone_test():
   tello.socket_send(command_list.land)
   tello.socket_close()
   del tello  
+
+class Tello:
+  def __init__(self):
+    self.battery = 0
+    self.attitude = {'pitch':0, 'roll':0, 'yaw':0}
+    self.tof = 0
+    self.height = 0
+    self.acceleration = {'agx':0, 'agy':0, 'agz':0}
+    self.speed = {'vgx':0, 'vgy':0, 'vgz':0}
+    self.barometer = 0
+  
+  def set_battery(self, battery):
+    self.battery = battery
+
+  def set_attitude(self, pitch, roll, yaw):
+    self.attitude['pitch'] = pitch
+    self.attitude['roll'] = roll
+    self.attitude['yaw'] = yaw
+  
+  def set_tof(self, tof):
+    self.tof = tof
+  
+  def set_height(self, height):
+    self.height = height
+  
+  def set_acceleration(self, agx, agy, agz):
+    self.acceleration['agx'] = agx
+    self.acceleration['agy'] = agy
+    self.acceleration['agz'] = agz
+  
+  def set_speed(self, vgx, vgy, vgz):
+    self.speed['vgx'] = vgx
+    self.speed['vgy'] = vgy
+    self.speed['vgz'] = vgz
+    
+  def set_barometer(self, baro):
+    self.barometer = baro
+
+  def get_state(self):
+    return self.battery, self.attitude, self.tof, self.height, self.acceleration, self.speed
 
 if __name__ == "__main__":
   drone_test()
